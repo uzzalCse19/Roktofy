64467720b2fa6ea0aa9b1afb68a76a10774b212f
uzzalCse19 <uzzalhasan7878@gmail.com>
2025-05-10 00:42:13 +0600

    OK

--- users/signals.py
+++ users/signals.py
@@ -1,3 +1,4 @@
+
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 from .models import User, UserProfile
@@ -5,4 +6,6 @@ from .models import User, UserProfile
 @receiver(post_save, sender=User)
 def create_user_profile(sender, instance, created, **kwargs):
     if created:
-        UserProfile.objects.create(user=instance)
+        # Check if a profile already exists before creating
+        UserProfile.objects.get_or_create(user=instance)
+
