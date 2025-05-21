7d114c00ae891f50a0782c14581ec900a891ab14
uzzalCse19 <uzzalhasan7878@gmail.com>
2025-05-21 15:31:27 +0600

    Setting Time Zone

--- roktofy/settings.py
+++ roktofy/settings.py
@@ -18,8 +18,6 @@ import cloudinary
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-TIME_ZONE = 'Asia/Dhaka'
-USE_TZ = True
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
