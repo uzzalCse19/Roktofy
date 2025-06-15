a759c609f73bd92330bce2378c68005e03967e17
uzzalCse19 <uzzalhasan7878@gmail.com>
2025-06-15 23:17:36 +0600

    OK

--- roktofy/settings.py
+++ roktofy/settings.py
@@ -93,7 +93,6 @@ WSGI_APPLICATION = 'roktofy.wsgi.app'
 CORS_ALLOWED_ORIGINS = [
     'http://localhost:5173',
     'https://roktofy-frontend.vercel.app',
-    'https://roktofy-frontend-oa9pilkxi-md-uzzals-projects.vercel.app',
 ]
 
 INTERNAL_IPS = [
